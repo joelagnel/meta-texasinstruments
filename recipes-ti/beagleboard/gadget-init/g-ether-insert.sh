526131d22cfd0e9176f4e7bcb23914cf9aeee91e
Joel A Fernandes <joelagnel@ti.com>
2011-11-09 11:00:39 -0600

    Fix g-ether with review comments
    Signed-off-by: Joel A Fernandes <joelagnel@ti.com>

--- recipes-ti/beagleboard/gadget-init/g-ether-insert.sh
+++ recipes-ti/beagleboard/gadget-init/g-ether-insert.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -a /sys/devices/platform/omap/ti81xx-usbss/musb-hdrc.0/gadget/lun0/file ]
 then
-	x=`cat /sys/devices/platform/omap/ti81xx-usbss/musb-hdrc.0/gadget/lun0/file`
-	if [ "$x" == ""  ]
+	x=$(cat /sys/devices/platform/omap/ti81xx-usbss/musb-hdrc.0/gadget/lun0/file)
+	if [ -z "$x" ]
 	then
 		/bin/systemctl stop storage-gadget-init.service
 		/bin/systemctl start network-gadget-init.service
--- recipes-ti/beagleboard/gadget-init/g-storage-reinsert.sh
+++ recipes-ti/beagleboard/gadget-init/g-storage-reinsert.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 /bin/systemctl stop storage-gadget-init.service
 /bin/systemctl stop network-gadget-init.service
 /bin/systemctl start storage-gadget-init.service
